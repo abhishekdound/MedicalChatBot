8c30be58cc00947d398a832f34d83e6f789fe412
Abhishek <abhishekdound@gmail.com>
2026-02-22 14:38:02 +0530

    retriever added

---
+++ src/embedding_helper.py
@@ -0,0 +1,35 @@
+import os
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+class Embedder:
+    def __init__(self):
+        self.embeddings = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+        self.folder_path = "vectorstore"
+        self.index_name = "medical_book_index"
+
+    def get_retriever(self, documents):
+
+        if os.path.exists(self.folder_path):
+            vectorstore = FAISS.load_local(
+                folder_path=self.folder_path,
+                embeddings=self.embeddings,
+                index_name=self.index_name,
+                allow_dangerous_deserialization=True  # required in new versions
+            )
+
+        else:
+            vectorstore = FAISS.from_documents(
+                documents=documents,
+                embedding=self.embeddings
+            )
+
+            vectorstore.save_local(
+                folder_path=self.folder_path,
+                index_name=self.index_name
+            )
+
+        return vectorstore.as_retriever(search_kwargs={"k":3})
