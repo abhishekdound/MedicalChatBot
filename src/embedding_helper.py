1b3e3409f136cb3ee1a5fd94b929651e3d9aed83
Abishek Dound <145567841+abhishekdound@users.noreply.github.com>
2026-02-22 17:02:58 +0530

    Switch to FastEmbedEmbeddings for embedding
    Replaced HuggingFaceEmbeddings with FastEmbedEmbeddings and removed unnecessary document chunking logic.

--- src/embedding_helper.py
+++ src/embedding_helper.py
@@ -1,37 +1,23 @@
 import os
 from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
-from .chunkin_helper import chunk_document
+from langchain_community.embeddings import FastEmbedEmbeddings
 
 
 class Embedder:
     def __init__(self):
-        self.embeddings = HuggingFaceEmbeddings(
-            model_name="sentence-transformers/all-MiniLM-L6-v2"
-        )
+        self.embeddings = FastEmbedEmbeddings()
         self.folder_path = "vectorstore"
         self.index_name = "medical_book_index"
-        self.documents=chunk_document()
 
     def get_retriever(self):
 
-        if os.path.exists(self.folder_path):
-            vectorstore = FAISS.load_local(
-                folder_path=self.folder_path,
-                embeddings=self.embeddings,
-                index_name=self.index_name,
-                allow_dangerous_deserialization=True  # required in new versions
-            )
+        
+        vectorstore = FAISS.load_local(
+            folder_path=self.folder_path,
+            embeddings=self.embeddings,
+            index_name=self.index_name,
+            allow_dangerous_deserialization=True  # required in new versions
+        )
 
-        else:
-            vectorstore = FAISS.from_documents(
-                documents=self.documents,
-                embedding=self.embeddings
-            )
-
-            vectorstore.save_local(
-                folder_path=self.folder_path,
-                index_name=self.index_name
-            )
 
         return vectorstore.as_retriever(search_kwargs={"k":3})
